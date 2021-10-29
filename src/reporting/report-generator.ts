99c761a970864050a53999c81ac61b8f227cc2d5
Luis Zanella <luiscontreraszane@gmail.com>
2021-10-29 16:16:31 -0500

    Test Reporting functionality added

---
+++ src/reporting/report-generator.ts
@@ -0,0 +1,25 @@
+import * as path from 'path';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const report = require('multiple-cucumber-html-reporter');
+
+const projectName = path.basename(__dirname);
+const projectVersion = process.env.npm_package_version;
+const reportGenerationTime = new Date().toISOString();
+report.generate({
+  customData: {
+    data: [
+      { label: 'Project', value: `${projectName}` },
+      { label: 'Release', value: `${projectVersion}` },
+      { label: 'Report Generation Time', value: `${reportGenerationTime}` },
+    ],
+    title: 'Run info',
+  },
+  disableLog: true,
+  displayDuration: true,
+  durationInMS: true,
+  jsonDir: 'src/reporting/json',
+  openReportInBrowser: true,
+  reportName: 'Report Swag_Labs',
+  reportPath: 'reports',
+});
---
+++ test/Indextest.spec.ts
@@ -0,0 +1,31 @@
+import { Selector } from 'testcafe';
+
+fixture `Swag_Labs Unit Test`
+    .page `http://devexpress.github.io/testcafe/example`;
+
+    test('My first test', async t => {
+        await t
+            .typeText('#developer-name', 'John Smith')
+            .click('#submit-button');
+    });
+
+
+    test('My second test', async t => {
+        await t
+            .typeText('#developer-name', 'John Smith')
+            .click('#submit-button');
+    
+        const articleHeader = await Selector('.result-content').find('h1');
+    
+        // Obtain the text of the article header
+        let headerText = await articleHeader.innerText;
+    });
+
+    test('My third test', async t => {
+        await t
+            .typeText('#developer-name', 'John Smith')
+            .click('#submit-button')
+    
+            // Use the assertion to check if the actual header text is equal to the expected one
+            .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!');
+    });
