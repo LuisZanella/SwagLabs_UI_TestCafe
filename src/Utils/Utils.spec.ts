1303dda36d9d0f1944b10a1af1a6657ad2540251
Luis Zanella <luiscontreraszane@gmail.com>
2021-10-31 02:40:12 -0600

    Added General Exceptions and Utils

--- src/Utils/Utils.spec.ts
+++ src/Utils/Utils.spec.ts
@@ -1,3 +1,15 @@
 import { ClientFunction } from 'testcafe';
+import { NotANumberException } from '../Utils/Exceptions.spec';
 
 export const getClientLocation = ClientFunction(() => document.location.href);
+
+export const isNumber = (n) => {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+export const getInnerTextNumberFromItem =  async (item: Selector,element:string) => {
+    let value = await item.find(element).innerText;
+    value = value.replace(/\$/g,'');
+    if(!isNumber(value)) throw new NotANumberException();
+    return parseFloat(value);
+}
