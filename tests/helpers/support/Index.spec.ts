baf916cad380b3d01adb60c0abf0da40e2d1e071
Luis Zanella <luiscontreraszane@gmail.com>
2021-11-03 13:42:58 -0600

    Identation on userTestData

--- tests/helpers/support/Index.spec.ts
+++ tests/helpers/support/Index.spec.ts
@@ -2,6 +2,6 @@ import ConstantData from '@helpers/support/ConstantData.spec';
 import {
   multipleObjectAssertion,
   isNextItemHigherAssertion,
-} from '@helpers/support//Assertion.spec';
+} from '@helpers/support/Assertion.spec';
 
 export { ConstantData, multipleObjectAssertion, isNextItemHigherAssertion };
